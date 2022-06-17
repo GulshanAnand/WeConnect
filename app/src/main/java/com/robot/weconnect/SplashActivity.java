cbe755c378bbf76ffc80da7915c732c9ce2d1126
Gulshan Anand <gulshan2052@gmail.com>
2022-06-17 20:54:36 +0530

    first commit

---
+++ app/src/main/java/com/robot/weconnect/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.robot.weconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+                // close this activity
+                finish();
+            }
+        }, 2000);
+    }
+}
